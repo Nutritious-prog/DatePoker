73f7786b7f731b7c70bef507b0de3bc9df1f05b3
Nutritious-prog <artur.wlodarczyk12@gmail.com>
2025-03-20 17:10:30 +0100

    Adding backend project

---
+++ dp_backend/src/main/java/com/datepoker/dp_backend/DpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.datepoker.dp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DpBackendApplication.class, args);
+    }
+
+}
---
+++ dp_backend/src/test/java/com/datepoker/dp_backend/DpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datepoker.dp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DpBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
